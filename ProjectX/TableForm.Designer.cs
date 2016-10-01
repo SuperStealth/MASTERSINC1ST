5c0e3fe1a558b131a64d3a202e8e0fbdb03dbfe6
SuperStealth <wokep95@mail.ru>
2016-10-01 18:43:54 +0300

    хых здарова

--- ProjectX/TableForm.Designer.cs
+++ ProjectX/TableForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 302);
+            this.ClientSize = new System.Drawing.Size(366, 265);
             this.Name = "TableForm";
             this.Text = "TableForm";
             this.ResumeLayout(false);
